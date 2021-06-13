795e047f02a6c34fc75ff257309c3da9e876593c
fatih40 <baserfatih40@gmail.com>
2021-06-13 12:11:02 +0200

    continuos

--- app/src/main/java/com/suleymanoner/carapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/suleymanoner/carapp/fragments/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.suleymanoner.carapp
+package com.suleymanoner.carapp.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.suleymanoner.carapp.R
 
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
